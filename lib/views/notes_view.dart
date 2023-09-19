e0dedfe6dc960bbf4e501b7b273ad1ca2d310f9b
mkhaledharb@gmail.com <mkhaledharb@gmail.com>
2023-09-19 20:34:11 +0300

    show modal bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/widget/add_note_bottom_sheet.dart';
 import 'package:notes/views/widget/note_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -9,6 +10,12 @@ class NotesView extends StatelessWidget {
     return  Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: (){
+          showModalBottomSheet(
+              context: context,
+              builder: (context){
+          return  const AddNoteBottomSheet();
+              }
+          );
         },
         child:const Icon(Icons.add),
       ),
@@ -16,3 +23,4 @@ class NotesView extends StatelessWidget {
     );
   }
 }
+
