1bef6f07f5b58b069093dcd6da8b623cd1ac121d
mkhaledharb@gmail.com <mkhaledharb@gmail.com>
2023-09-19 14:51:45 +0300

    create  custom_note_item

--- lib/views/widget/note_view_body.dart
+++ lib/views/widget/note_view_body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes/views/widget/custom_note_item.dart';
 
 import 'custom_app_bar.dart';
 
@@ -8,14 +11,16 @@ class NoteViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding:  EdgeInsets.symmetric(horizontal: 24),
       child:   Column(
         children: [
           SizedBox(height: 40,),
           CustomAppBa(),
+          NoteItem(),
         ],
       ),
     );
   }
 }
+
 
