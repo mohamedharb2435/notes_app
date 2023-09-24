64dfcc2c161cec0ca041b9a9dd3fa6b8ed66a56b
mkhaledharb@gmail.com <mkhaledharb@gmail.com>
2023-09-24 13:08:35 +0300

    create cubit

--- lib/views/widget/add_note_bottom_sheet.dart
+++ lib/views/widget/add_note_bottom_sheet.dart
@@ -66,7 +66,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
               }
             },
           ),
-         const     SizedBox(height: 20,),
+         const SizedBox(height: 20,),
         ],
       ),
     );
